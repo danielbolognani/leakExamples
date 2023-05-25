ce9a63bb32c55f0d6fc8f39c01547728a57684a7
Daniel O. Bolognani <daniel.bolognani@totvs.com.br>
2023-05-25 14:58:10 -0300

    Adicionando casos de leak

--- Leaks/Example1/Example1.cpp
+++ Leaks/Example1/Example1.cpp
@@ -5,16 +5,10 @@
 #include "tAutoPointer.h"
 #include "classeLeak1.h"
 
-Leak* createLeak(int nVal) {
-  return new Leak(nVal);
-}
-
-
-Leak* createLeak2(int nVal) {
-  Leak* tmp = new Leak(nVal);
-  delete tmp;
-  return tmp;
-}
+void funct1();
+void funct2();
+Leak* createLeak(int nVal);
+Leak* createLeak2(int nVal);
 
 
 //Resultado esperado:
@@ -37,6 +31,8 @@ nVal = 13 sVal = Leak13
 nVal = 14 sVal = Leak14
 nVal = 15 sVal = Leak15
 nVal = 16 sVal = Leak16
+123456789
+123456789 abcdefghij
 */
 
 int main()
@@ -44,13 +40,18 @@ int main()
     std::cout << "Vamos processar!\n";
     {
       Leak* leak1 = new Leak(1);
+
       Leak leak2(2);
       leak2.newStr("Leak2");
+
       Leak* leak3 = createLeak(3);
+
       Leak* leak4 = createLeak2(4);
-      //leak4->newStr("Leak4");
+      //leak4->newStr("Leak4"); //GPF!
+
       std::vector<Leak*> listaVetor;
       listaVetor.push_back(leak3);
+
       for (int i = 5; i < 15; i++) {
         listaVetor.push_back(new Leak(i));
         std::string sValor = "Leak";
@@ -60,6 +61,7 @@ int main()
 
       tAutoPointer<Leak> leak15(new Leak(15));
       std::string* sRet15 = leak15->newStr("Leak15");
+
       tAutoPointer<Leak> leak16(new Leak(16));
       std::string* sRet16 = leak16->newStr("Leak16");
 
@@ -69,13 +71,21 @@ int main()
       std::string* sRet3 = leak3->newStr("Leak3");
 
       leak1->printVal();
+
       leak2.printVal();
+
       leak3->printVal();
-      //leak4->printVal();
+
+      //leak4->printVal(); //GPF!
       for (Leak* tmp : listaVetor)
         tmp->printVal();
+
       leak15->printVal();
+
       leak16->printVal();
+
+      funct1();
+      funct2();
 
       delete sRet3;
       delete leak1;
@@ -87,4 +97,30 @@ int main()
 }
 
 
+void funct1() {
+  std::string* teste = new std::string;
+  *teste = "123456789";
+  std::cout << *teste << "\n";
+}
 
+void funct2() {
+  std::string* teste = new std::string;
+  std::string* abc = new std::string("abcdefghij");
+  *teste = "123456789";
+  std::cout << *teste << " ";
+  teste = abc;
+  std::cout << *teste;
+  delete teste;
+}
+
+
+Leak* createLeak(int nVal) {
+  return new Leak(nVal);
+}
+
+
+Leak* createLeak2(int nVal) {
+  Leak* tmp = new Leak(nVal);
+  delete tmp;
+  return tmp;
+}
